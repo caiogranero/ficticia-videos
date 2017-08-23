bbf263c5d5290cabae1ec313a13746234f5848b7
Caio Granero <caio.granero@icolabora.com.br>
2017-08-23 20:43:26 -0300

    videolist component

---
+++ src/components/videos/VideosList.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <!-- <div data-v-2246501d="" class="><div data-v-2246501d="" class="md-card md-theme-default"><div data-v-2246501d="" class="md-card-header md-card-header-flex"><div data-v-2246501d="" class="md-card-media md-custom-big md-big"><img data-v-2246501d="" src="https://i.ytimg.com/vi/R5HlB3fEV2o/mqdefault.jpg"></div> <div data-v-2246501d="" class="video-info"><div data-v-2246501d="" class="md-card-header-text"><div data-v-2246501d="" class="md-title-videos"><p data-v-2246501d="">Sonic Funeral</p></div> <div data-v-2246501d="" class="md-views-videos"><i data-v-2246501d="" aria-hidden="true" class="md-icon md-size-1x icon-pointer md-theme-default material-icons">remove_red_eye</i> 100 views</div></div></div></div></div></div><div data-v-2246501d="" class="md-layout md-videos-custom md-flex-xsmall-100 md-flex-small-100 md-flex-medium-60 md-flex-large-33"><div data-v-2246501d="" class="md-card md-theme-default"><div data-v-2246501d="" class="md-card-header md-card-header-flex"><div data-v-2246501d="" class="md-card-media md-custom-big md-big"><img data-v-2246501d="" src="https://i.ytimg.com/vi/Rh9ytBuHnck/mqdefault.jpg"></div> <div data-v-2246501d="" class="video-info"><div data-v-2246501d="" class="md-card-header-text"><div data-v-2246501d="" class="md-title-videos"><p data-v-2246501d="">Super Mario vs Sonic the Hedgeho...</p></div> <div data-v-2246501d="" class="md-views-videos"><i data-v-2246501d="" aria-hidden="true" class="md-icon md-size-1x icon-pointer md-theme-default material-icons">remove_red_eye</i> 100 views</div></div></div></div></div></div><div data-v-2246501d="" class="md-layout md-videos-custom md-flex-xsmall-100 md-flex-small-100 md-flex-medium-60 md-flex-large-33"><div data-v-2246501d="" class="md-card md-theme-default"><div data-v-2246501d="" class="md-card-header md-card-header-flex"><div data-v-2246501d="" class="md-card-media md-custom-big md-big"><img data-v-2246501d="" src="https://i.ytimg.com/vi/ltbR9zj0Tuc/mqdefault.jpg"></div> <div data-v-2246501d="" class="video-info"><div data-v-2246501d="" class="md-card-header-text"><div data-v-2246501d="" class="md-title-videos"><p data-v-2246501d="">EVOLUTION OF SONIC THE HEDGEHOG ...</p></div> <div data-v-2246501d="" class="md-views-videos"><i data-v-2246501d="" aria-hidden="true" class="md-icon md-size-1x icon-pointer md-theme-default material-icons">remove_red_eye</i> 100 views</div></div></div></div></div></div><div data-v-2246501d="" class="md-layout md-videos-custom md-flex-xsmall-100 md-flex-small-100 md-flex-medium-60 md-flex-large-33"><div data-v-2246501d="" class="md-card md-theme-default"><div data-v-2246501d="" class="md-card-header md-card-header-flex"><div data-v-2246501d="" class="md-card-media md-custom-big md-big"><img data-v-2246501d="" src="https://i.ytimg.com/vi/FHMzaqJBVYg/mqdefault.jpg"></div> <div data-v-2246501d="" class="video-info"><div data-v-2246501d="" class="md-card-header-text"><div data-v-2246501d="" class="md-title-videos"><p data-v-2246501d="">Sonic and the Black Knight All C...</p></div> <div data-v-2246501d="" class="md-views-videos"><i data-v-2246501d="" aria-hidden="true" class="md-icon md-size-1x icon-pointer md-theme-default material-icons">remove_red_eye</i> 100 views</div></div></div></div></div></div><div data-v-2246501d="" class="md-layout md-videos-custom md-flex-xsmall-100 md-flex-small-100 md-flex-medium-60 md-flex-large-33"><div data-v-2246501d="" class="md-card md-theme-default"><div data-v-2246501d="" class="md-card-header md-card-header-flex"><div data-v-2246501d="" class="md-card-media md-custom-big md-big"><img data-v-2246501d="" src="https://i.ytimg.com/vi/6KhFYLrfFg8/mqdefault.jpg"></div> <div data-v-2246501d="" class="video-info"><div data-v-2246501d="" class="md-card-header-text"><div data-v-2246501d="" class="md-title-videos"><p data-v-2246501d="">DOES THIS GAME MARK THE END OF T...</p></div> <div data-v-2246501d="" class="md-views-videos"><i data-v-2246501d="" aria-hidden="true" class="md-icon md-size-1x icon-pointer md-theme-default material-icons">remove_red_eye</i> 100 views</div></div></div></div></div></div></div></div> -->
+  <md-layout class="outside-space">
+    <md-layout md-flex="70">
+      <h1>
+        <md-icon class="md-size-2x">play_circle_outline</md-icon>
+        Fictícia videos
+      </h1>
+    </md-layout>
+    <md-layout class="md-videos-custom" md-flex-xsmall="100" md-flex-small="100" md-flex-medium="60" md-flex-large="33">
+
+    </md-layout>
+  </md-layout> 
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
